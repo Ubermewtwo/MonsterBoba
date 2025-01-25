b0a9303647febc1d145aa1ed461eb7c4a51af80d
Arcadiu <bmartrodri@gmail.com>
2025-01-25 23:03:40 +0100

    color y sprite en tooltip

--- Assets/Data/Scripts/Monsters/MonsterPartTooltip.cs
+++ Assets/Data/Scripts/Monsters/MonsterPartTooltip.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MonsterPartTooltip : MonoBehaviour
 {
@@ -11,6 +12,20 @@ public class MonsterPartTooltip : MonoBehaviour
     [SerializeField] private TextMeshProUGUI bobaTeaPartText;
     [SerializeField] private List<TextMeshProUGUI> flavoursText;
 
+    [SerializeField] private Color colorBitter;
+    [SerializeField] private Color colorSpicy;
+    [SerializeField] private Color colorSweet;
+    [SerializeField] private Color colorSalt;
+
+    private Color color;
+
+    [SerializeField] private Sprite spriteBitter;
+    [SerializeField] private Sprite spriteSpicy;
+    [SerializeField] private Sprite spriteSweet;
+    [SerializeField] private Sprite spriteSalt;
+
+    [SerializeField] private Image imageInTooltip;
+
     private void OnEnable()
     {
         nameText.text = monsterPart.Name;
@@ -18,13 +33,42 @@ public class MonsterPartTooltip : MonoBehaviour
         for (int i = 0; i < monsterPart.Flavours.Count; i++)
         {
             flavoursText[i].gameObject.SetActive(true);
-            flavoursText[i].text = monsterPart.Flavours.Keys[i].ToString() + ": " + monsterPart.Flavours.Values[i].ToString();
+            ChooseColorAndImageForText(i);
+
+            flavoursText[i].text = "<color=#" + ColorUtility.ToHtmlStringRGB(color) + ">" + monsterPart.Flavours.Keys[i].ToString() +  ": " + monsterPart.Flavours.Values[i].ToString() + "</color>";
         }
 
         for (int i = monsterPart.Flavours.Count; i < flavoursText.Count; i++)
         {
             flavoursText[i].gameObject.SetActive(false);
             flavoursText[i].text = "";
+        }
+    }
+
+    private void ChooseColorAndImageForText(int indice)
+    {
+        switch (monsterPart.Flavours.Keys[indice])
+        {
+            case FlavourType.Bitter:
+                color = colorBitter;
+                imageInTooltip.sprite = spriteBitter;
+                break;
+            case FlavourType.Spicy:
+                color = colorSpicy;
+                imageInTooltip.sprite = spriteSpicy;
+                break;
+            case FlavourType.Sweet:
+                color = colorSweet;
+                imageInTooltip.sprite = spriteSweet;
+                break;
+            case FlavourType.Salty:
+                color = colorSalt;
+                imageInTooltip.sprite = spriteSalt;
+                break;
+            default:
+                color = colorSweet;
+                imageInTooltip.sprite = spriteSweet;
+                break;
         }
     }
 }
