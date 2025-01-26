2f150c08528315236772f0875e500292aa1d00df
NatillasConNata <nataliamartinezclemente@hotmail.es>
2025-01-26 13:00:42 +0100

    Escena Tutorial casi completa

---
+++ Assets/Data/Scripts/TutoScene/TutorialButtonsManager.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialButtonsManager : MonoBehaviour
+{
+    public List<GameObject> DialogImages = new List<GameObject>();
+    public List<Button> DialogButtons = new List<Button>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextDialog()
+    {
+        
+    }
+
+}
