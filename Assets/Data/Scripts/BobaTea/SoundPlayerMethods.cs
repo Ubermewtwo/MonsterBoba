700651e99f560f8ba07dda46c0e7217df87a7ff2
Ubermewtwo <86959459+Ubermewtwo@users.noreply.github.com>
2025-01-26 12:24:51 +0100

    Tooltip por encima del panel y otras cosas que no recuerdo

--- Assets/Data/Scripts/BobaTea/SoundPlayerMethods.cs
+++ Assets/Data/Scripts/BobaTea/SoundPlayerMethods.cs
@@ -3,9 +3,21 @@ using UnityEngine;
 public class SoundPlayerMethods : MonoBehaviour
 {
     [SerializeField] private AudioClipList discardSounds;
+    [SerializeField] private AudioClipList hoverSounds;
+    [SerializeField] private AudioClipList clickSounds;
 
     public void PlayDiscardSound()
     {
         discardSounds.PlayAtPointRandom(transform.position);
+    }
+
+    public void PlayHoverSound()
+    {
+        hoverSounds.PlayAtPointRandom(transform.position);
+    }
+
+    public void PlayClickSound()
+    {
+        clickSounds.PlayAtPointRandom(transform.position);
     }
 }
